49619314b5a3e3a32420b3308d9466e40bd99af7
Julia Ajpert <jajpert@gmail.com>
2024-08-10 22:28:43 -0400

    feat(institutions): adding features for institutions

--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const paises = require('./controller/paises');
 const enderecos = require('./controller/enderecos');
+const instituicoes = require('./controller/instituicoes');
 
 const rotas = express();
 
 rotas.get('/enderecos/:cep', enderecos.obterEndereco);
 rotas.get('/paises', paises.obterPaises);
+
+rotas.post('/cadastrarInst', instituicoes.cadastrarInst);
+rotas.patch('/editarStatusInst/:id', instituicoes.ativarInst);
+rotas.get('/instituicoes', instituicoes.obterInst);
+rotas.put('/editarInst/:id', instituicoes.editarInst);
 
 module.exports = rotas;
